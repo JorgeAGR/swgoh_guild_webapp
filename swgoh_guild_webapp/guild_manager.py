e9064c10f06a507ab05ba307443b584a052c1ae8
Jorge A. Garcia <jorgeagr@97gmail.com>
2023-10-22 14:09:48 -0600

    abstracted base file manager

--- swgoh_guild_webapp/guild_manager.py
+++ swgoh_guild_webapp/guild_manager.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from abc import ABC, abstractmethod
 from typing import Self, Any
 import requests
 import pandas as pd
@@ -7,16 +8,37 @@ import os
 import json
 from swgoh_guild_webapp.swgoh_commlink_fetcher import SwgohCommlinkFetcher
 from swgoh_guild_webapp.entities import Guild, Player, CharacterRoster, Character
-from swgoh_guild_webapp.raid import RaidResults
+from swgoh_guild_webapp.raid import RaidResults, GuildRaidResults
 
 '''
 Need to add player data caching
 '''
+@dataclass
+class GuildFileManager(ABC):
+    guild_id: str
+
+    @abstractmethod
+    def read_latest_request(self, name: str) -> dict[str, Any] or None:
+        return
+    
+    @abstractmethod
+    def write_latest_request(self, request: dict[str, Any], name: str, time: datetime.datetime) -> None:
+        return
+    
+    @abstractmethod
+    def when_latest_request(self, name: str) -> datetime.datetime:
+        return
+
 
 @dataclass
-class GuildFileManager:
+class GuildLocalFileManager(GuildFileManager):
     guild_id: str
     out_dir: os.PathLike
+    archive: bool = True
+    '''
+    archive : bool
+        If True, will save the current latest data requests into an archive directory.
+    '''
 
     def __post_init__(self) -> None:
         os.makedirs(self.guild_dir, exist_ok=True)
@@ -51,23 +73,30 @@ class GuildFileManager:
             file.write(json.dumps(request, indent=4))
         return
     
-    def find_latest_request(self, name: str) -> str or None:
+    def when_latest_request(self, name: str) -> datetime.datetime or None:
         latest_files = os.listdir(self.latest_dir)
         latest_files.sort(key=lambda x: name in x, reverse=True)
-        file_name = None
+        file_timestamp = None
         if len(latest_files) > 0 and (name in latest_files[0]): # this is to handle the no file case
-            file_name = latest_files[0]
-        return file_name
+            file_timestamp = int(latest_files[0].split('_')[-1])
+        return datetime.datetime.fromtimestamp(file_timestamp)
+    
+    def _build_latest_request_file_name(self, name: str) -> str:
+        latest_date = self.when_latest_request(name)
+        latest_name = None
+        if latest_date is not None:
+            latest_name = f'{name}_{int(latest_date.timestamp())}'
+        return latest_name
     
     def read_latest_request(self, name: str) -> dict[str, Any] or None:
-        latest_name = self.find_latest_request(name)
+        latest_name = self._build_latest_request_file_name(name)
         if latest_name is not None:
             return self._read_request_json(os.path.join(self.latest_dir, latest_name))
         else:
             return None
     
-    def archive_latest_request(self, name: str) -> None:
-        latest_name = self.find_latest_request(name)
+    def _archive_latest_request(self, name: str) -> None:
+        latest_name = self._build_latest_request_file_name(name)
         if latest_name is not None:
             latest_path = os.path.join(self.latest_dir, latest_name)
             latest_request = self._read_request_json(latest_path)
@@ -79,7 +108,7 @@ class GuildFileManager:
         return
 
     def write_latest_request(self, request: dict[str, Any], name: str, time: datetime.datetime) -> None:
-        self.archive_latest_request(name)
+        self._archive_latest_request(name)
         latest_path = os.path.join(self.latest_dir, f'{name}_{round(time.timestamp())}')
         self._write_request_json(request, latest_path)
         return
@@ -90,12 +119,13 @@ class GuildManager:
     fetcher: SwgohCommlinkFetcher
     guild_id: str
     file_manager: GuildFileManager
-    refresh_hours: int = 6
+    refresh_hours: int = 24
     '''
     refresh_hours : hours after the latest data request is considered stale
     '''
 
     def __post_init__(self) -> None:
+        self.get_latest_data()
         self.guild = self.build_guild()
         self.latest_raid = self.build_raid()
 
@@ -104,11 +134,11 @@ class GuildManager:
         return ['guild_request', 'raid_result', 'members_request']
 
     def latest_request_time(self, name: str):
-        latest_timestamp = file_manager.find_latest_request(name)
-        if latest_timestamp is None:
+        latest_date = self.file_manager.when_latest_request(name)
+        if latest_date is None:
             return datetime.datetime.fromtimestamp(0)
         else:
-            return datetime.datetime.fromtimestamp(int(latest_timestamp.split('_')[-1]))
+            return latest_date
 
     def _request_guild_data(self) -> dict[str, Any]:
         guild_request = self.fetcher.get_guild_data(self.guild_id)
@@ -146,17 +176,25 @@ class GuildManager:
         return
 
     def build_guild(self) -> Guild:
-        self.get_latest_data()
         members = [Player.build_player(self._latest_member_request[member['playerId']], member['galacticPower']) for member in self._latest_guild_request['member']]
         return Guild.build_guild(self._latest_guild_request, members)
     
     def build_raid(self) -> RaidResults:
-        self.get_latest_data()
-        return RaidResults.build_results(self._latest_raid_result)
+        raid_results = RaidResults.build_results(self._latest_raid_result)
+        return GuildRaidResults(self.guild, raid_results)
+    
 
+class GuildTags:
+    guild: Guild
+
+    def __post_init__(self):
+        
+        return
     
 if __name__ == '__main__':
     guild_id = 'dYXen85NS3SCrdllQ4lAEg'
     fetcher = SwgohCommlinkFetcher()
-    file_manager = GuildFileManager(guild_id, '/mnt/d/data/swgoh/guilds')
+    file_manager = GuildLocalFileManager(guild_id, '/mnt/d/data/swgoh/guilds')
     manager = GuildManager(fetcher, guild_id, file_manager, refresh_hours=6)
+    manager.latest_raid.scores_df.to_csv('raid_scores.csv')
+    print(0)
