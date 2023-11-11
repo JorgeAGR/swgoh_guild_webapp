10eafcbeb19e6f004a4e6134b94862f1d157b170
Jorge A. Garcia <jorgeagr97@gmail.com>
2023-11-10 23:01:34 -0700

    webapp updates

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -5,18 +5,24 @@ from swgoh_guild_webapp.entities import Guild
 
 
 def draw_guild_roster_view() -> None:
-    st.header(f'Viewing roster for {st.session_state.guild_manager.guild.name}')
-    st.data_editor(data=st.session_state.guild_manager.guild.data,
-                   disabled=['Allycode', 'Name', 'GP'],
-                   hide_index=True,
-                   use_container_width=True)
-    return
+    st.header(f'Viewing roster for {st.session_state.guild_id}')
+    summary_df = st.session_state.raid_data._raw_df.groupby('EndDate').sum().reset_index()
+    st.line_chart(summary_df, x='EndDate', y='Score')
+    return st.dataframe(data=st.session_state.raid_data.df,
+                        hide_index=True,
+                        column_config={
+                            'Score History': st.column_config.LineChartColumn(
+                                width='medium'
+                            )
+                        },
+                        use_container_width=True,
+                        height=(len(st.session_state.raid_data.df) + 1) * 35 + 3)
 
 if __name__ == '__main__':
     app_startup()
     add_sidebar()
 
-    guild = draw_guild_roster_view()
+    raid = draw_guild_roster_view()
 
     # central = st.empty()
     # with central.container():
